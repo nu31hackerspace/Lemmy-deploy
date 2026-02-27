814f4bd71ce5ea67d8291f8d0808bea6eb55832a
denchik <danil.mirzoev@gmail.com>
2026-02-28 01:22:46 +0200

    first commit

---
+++ render-lemmy-config.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -eu
+
+export POSTGRES_PASSWORD="$(cat /run/secrets/POSTGRES_PASSWORD)"
+export PICTRS_API_KEY="$(cat /run/secrets/PICTRS_API_KEY)"
+export LEMMY_ADMIN_USERNAME="$(cat /run/secrets/LEMMY_ADMIN_USERNAME)"
+export LEMMY_ADMIN_PASSWORD="$(cat /run/secrets/LEMMY_ADMIN_PASSWORD)"
+export LEMMY_ADMIN_EMAIL="$(cat /run/secrets/LEMMY_ADMIN_EMAIL)"
+
+envsubst < /config/lemmy.hjson.template > /config/lemmy.hjson
