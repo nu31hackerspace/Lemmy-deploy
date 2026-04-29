e6e96aa50b2c48b82c3c049f5ddac8c67c5e4a8e
denchik <danil.mirzoev@gmail.com>
2026-04-29 21:10:12 +0300

    Switch forum deploy to docker compose

--- render-lemmy-config.sh
+++ render-lemmy-config.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -eu
 
-export POSTGRES_PASSWORD="$(cat /run/secrets/POSTGRES_PASSWORD)"
-export PICTRS_API_KEY="$(cat /run/secrets/PICTRS_API_KEY)"
-export LEMMY_ADMIN_USERNAME="$(cat /run/secrets/LEMMY_ADMIN_USERNAME)"
-export LEMMY_ADMIN_PASSWORD="$(cat /run/secrets/LEMMY_ADMIN_PASSWORD)"
-export LEMMY_ADMIN_EMAIL="$(cat /run/secrets/LEMMY_ADMIN_EMAIL)"
-
+: "${TEMPLATE_PATH:=./lemmy.hjson.template}"
+: "${OUTPUT_PATH:=./lemmy.generated.hjson}"
+: "${POSTGRES_PASSWORD:?POSTGRES_PASSWORD is required}"
+: "${PICTRS_API_KEY:?PICTRS_API_KEY is required}"
+: "${LEMMY_ADMIN_USERNAME:?LEMMY_ADMIN_USERNAME is required}"
+: "${LEMMY_ADMIN_PASSWORD:?LEMMY_ADMIN_PASSWORD is required}"
+: "${LEMMY_ADMIN_EMAIL:?LEMMY_ADMIN_EMAIL is required}"
 : "${LEMMY_HOSTNAME:?LEMMY_HOSTNAME is required}"
-: "${LEMMY_CONFIG_LOCATION:=/tmp/lemmy.hjson}"
 
-mkdir -p "$(dirname "${LEMMY_CONFIG_LOCATION}")"
-envsubst < /lemmy.hjson.template > "${LEMMY_CONFIG_LOCATION}"
+mkdir -p "$(dirname "${OUTPUT_PATH}")"
+perl -pe 's/\$\{(\w+)\}/exists $ENV{$1} ? $ENV{$1} : $&/ge' "${TEMPLATE_PATH}" > "${OUTPUT_PATH}"
